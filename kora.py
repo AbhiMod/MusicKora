4a004f0c789ac2fbc8d75e9acd007a5d6bd4f5b1
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-19 10:48:29 +0530

    Create kora.py

---
+++ kora.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && rm -rf MusicKora && git clone https://github.com/AbhiMod/MusicKora && cd MusicKora && python3 -m KoraMusic")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
